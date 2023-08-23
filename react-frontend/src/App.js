2eab552a7a4eeafdb8d45ad706f0a26b8f80e2bd
Ang Xiang Ying <xyang.2022@scis.smu.edu.sg>
2023-08-23 20:50:50 +0800

    second commit with basic login template in react

---
+++ react-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginComponent from './components/LoginComponent/LoginComponent';
+
+function App() {
+  return (
+    <div className="container">
+          <LoginComponent />
+    </div>
+  );
+}
+
+export default App;
