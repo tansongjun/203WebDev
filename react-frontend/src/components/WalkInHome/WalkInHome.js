1f52e77f99f6839df44acb33d8977144a2e35576
tansongjun <87587979+tansongjun@users.noreply.github.com>
2023-09-17 23:04:37 +0800

    Modified get queue number functionality such that user has to press a button first.

--- react-frontend/src/components/WalkInHome/WalkInHome.js
+++ react-frontend/src/components/WalkInHome/WalkInHome.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 function WalkInHome() {
   const [randomNumber, setRandomNumber] = useState(null);
   const [generatedNumbers, setGeneratedNumbers] = useState([]);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   const generateRandomNumber = () => {
     let newNumber;
@@ -11,20 +12,29 @@ function WalkInHome() {
     } while (generatedNumbers.includes(newNumber));
 
     setRandomNumber(newNumber);
-    setGeneratedNumbers([...generatedNumbers, newNumber]);
-  };
+    setGeneratedNumbers((previouslyGeneratedNumber) => [
+      ...previouslyGeneratedNumber,
+      newNumber,
+    ]);
 
-  // Automatically generate a random number when the component mounts
-  useEffect(() => {
-    generateRandomNumber();
-  }, []); // The empty dependency array ensures this runs only once
+    setButtonDisabled(true);
+  };
 
   return (
     <div>
       <h1>WALK IN HOME</h1>
       <div style={{ textAlign: "center", fontSize: "50px", marginTop: "20px" }}>
-        {randomNumber !== null ? randomNumber : "Generating..."}
+        {randomNumber !== null
+          ? "Your Queue Number: " + randomNumber
+          : "Register here"}
       </div>
+      <button
+        className="queue-button"
+        onClick={generateRandomNumber}
+        disabled={buttonDisabled} // Set the disabled attribute based on buttonDisabled state
+      >
+        Get Queue Number
+      </button>
     </div>
   );
 }
