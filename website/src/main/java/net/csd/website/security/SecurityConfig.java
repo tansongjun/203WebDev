97975d2cc9e40999e620d145f232a44ff3f1ff45
Williams <wongnicholas@outlook.sg>
2023-09-17 22:17:52 +0800

    Fixed the securityFilterChain
    [IMPT]
    - [fixed by zhi yun] Http Request Authentication
        - use mvc.pattern instead of antpattern
        - use hasAuthority instead of hasRole
    - Validation: whitelist username and password
    - bcrypt for password
    - fixed clicked front and back buttons; navigate(1)
    - Renamed "LoginPage" to "HomePage"
    
    [LESS IMPT]
    - CSS centered for frontend

--- website/src/main/java/net/csd/website/security/SecurityConfig.java
+++ website/src/main/java/net/csd/website/security/SecurityConfig.java
@@ -1,22 +1,17 @@
 package net.csd.website.security;
 
-import org.apache.coyote.http11.AbstractHttp11JsseProtocol;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-// import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-// import org.springframework.security.web.util.matcher.RequestMatcher;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
@@ -56,54 +51,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         http
-                .cors(cors -> cors.disable())
-                .csrf(csrf -> csrf.disable()) // CSRF protection is needed only for
-                // browser-based attacks
-                .formLogin(fg -> fg.disable())
-                .authenticationProvider(authenticationProvider()) // specifies the authentication provider for
-                                                                  // HttpSecurity
-                // security
-                // // .requestMatchers(new AntPathRequestMatcher("/api/v1/people", "POST"))
-                // );
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/v1/people/**")).hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
-                        .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/v1/people/**") ).hasAuthority("ROLE_ADMIN")
-                        .requestMatchers(mvc.pattern(HttpMethod.PUT, "/api/v1/people/**")).hasAuthority("ROLE_ADMIN")
-                        .requestMatchers(mvc.pattern(HttpMethod.DELETE, "/api/v1/people/**")).hasAuthority("ROLE_ADMIN")
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults())// allow Http Basic Authentication
+            .cors(cors -> cors.disable())
+            .csrf(csrf -> csrf.disable()) // CSRF protection is needed only for browser-based attacks
+            .formLogin(fg -> fg.disable())
+            .authenticationProvider(authenticationProvider()) // specifies the authentication provider for
+                                                                // HttpSecurity
+            // security
+            // // .requestMatchers(new AntPathRequestMatcher("/api/v1/people", "POST"))
+            // );
+            .authorizeHttpRequests((authz) -> authz
+                    .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/v1/people/**")).hasAnyAuthority("ROLE_ADMIN", "ROLE_USER")
+                    .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/v1/people/**") ).hasAuthority("ROLE_ADMIN")
+                    .requestMatchers(mvc.pattern(HttpMethod.PUT, "/api/v1/people/**")).hasAuthority("ROLE_ADMIN")
+                    .requestMatchers(mvc.pattern(HttpMethod.DELETE, "/api/v1/people/**")).hasAuthority("ROLE_ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults()) // allow Http Basic Authentication
         ;
         return http.build();
     }
-
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-    // Exception {
-    // http
-    // .cors(cors -> cors.disable())
-    // .csrf(csrf -> csrf.disable()) // CSRF protection is needed only for browser
-    // based attacks
-    // .authorizeHttpRequests((requests) -> requests
-    // .requestMatchers("/api/v1/people/**").permitAll()
-    // .anyRequest().authenticated()
-    // );
-    // // .authorizeHttpRequests(authorizeRequests ->
-    // // authorizeRequests
-    // // .requestMatchers("/api/v1/people/**").permitAll()
-    // // );
-    // // .authenticationProvider(authenticationProvider()); //specifies the
-    // authentication provider for HttpSecurity
-
-    // // .formLogin(logp -> logp.disable()); // Disable login form
-    // // .formLogin(formLogin -> // Enable login form
-    // // formLogin
-    // // .loginPage("/login")
-    // // .permitAll()
-    // // );
-    // // .rememberMe(withDefaults()); // Remember me function
-    // return http.build();
-    // }
 
     /**
      * @Bean annotation is used to declare a PasswordEncoder bean in the Spring
