8f284df79a56064989a9ebe3e5d42d6c8d92be43
tansongjun <87587979+tansongjun@users.noreply.github.com>
2023-11-03 18:59:16 +0800

    Implemented walkin patient workflow, linking frontend & backend

--- react-frontend/src/components/QTicketPage/QTicketPage.jsx
+++ react-frontend/src/components/QTicketPage/QTicketPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Axios from "axios";
-
 function QTicketPage() {
   const [auth, setAuth] = useState({
     user: sessionStorage.getItem("user"),
@@ -32,67 +31,86 @@ function QTicketPage() {
         response.data.dateTimeSlot.qticket
       ) {
         setQueueNumber(response.data.dateTimeSlot.qticket.ticketno);
-      } else {
-        // If not, create a new ticket
-        createNewTicket();
       }
 
       console.log("Queue Number:", response.data);
     } catch (error) {
       // Handle errors here
-      createNewTicket();
+      // createNewTicket();
       console.error("Error getting queue number:", error);
     }
   };
 
-  const createNewTicket = async () => {
-    try {
-      const response = await Axios.post(
-        `http://localhost:8080/api/v1/patients/${auth.personId}/getnewQ`,
-        null, // You can pass data as the second argument if needed
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          auth: {
-            username: `${auth.user}`,
-            password: `${auth.pwd}`,
-          },
-        }
-      ).then((response) => {
-        // Set the queue number in the state
-        if (
-          response.data &&
-          response.data.qticket &&
-          response.data.qticket.ticketno
-        ) {
-          setQueueNumber(response.data.qticket.ticketno);
-          window.location.reload(true);
-        }
+  // const createNewTicket = async () => {
+  //   try {
+  //     const response = await Axios.post(
+  //       `http://localhost:8080/api/v1/patients/${auth.personId}/getnewQ`,
+  //       null, // You can pass data as the second argument if needed
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         auth: {
+  //           username: `${auth.user}`,
+  //           password: `${auth.pwd}`,
+  //         },
+  //       }
+  //     ).then((response) => {
+  //       // Set the queue number in the state
+  //       if (
+  //         response.data &&
+  //         response.data.qticket &&
+  //         response.data.qticket.ticketno
+  //       ) {
+  //         setQueueNumber(response.data.qticket.ticketno);
+  //         window.location.reload(true);
+  //       }
 
-        console.log("New Ticket Created:", response.data);
-      });
-    } catch (error) {
-      // Handle errors here
-      const errorMessage = "Patient has already got a queue ticket for today";
-      // if (error.response.data.message === errorMessage) {
-      //   getQueueNumber();
-      // }
-      console.error(
-        "Error creating a new ticket:",
-        error.response.data.message
-      );
-    }
-  };
+  //       console.log("New Ticket Created:", response.data);
+  //       // setQueueNumber(response.data.qticket.ticketno);
 
+  //     });
+  //   } catch (error) {
+  //     // Handle errors here
+  //     const errorMessage = "Patient has already got a queue ticket for today";
+  //     // if (error.response.data.message === errorMessage) {
+  //     //   getQueueNumber();
+  //     // }
+  //     console.error(
+  //       "Error creating a new ticket:",
+  //       error.response.data.message
+  //     );
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getQueueNumber(); // Check if the user has a ticket number
+  // }, []);
   useEffect(() => {
-    getQueueNumber(); // Check if the user has a ticket number
-  }, []);
+    getQueueNumber(); // Initial fetch of the queue number
+
+    // Set up an interval to fetch the queue number at regular intervals
+    const intervalId = setInterval(() => {
+      getQueueNumber();
+    }, 500); // Adjust the interval duration as needed (e.g., fetch every 5 seconds)
+
+    // Clean up the interval when the component unmounts
+    return () => {
+    clearInterval(intervalId);
+    };
+    }, [auth.personId, auth.user, auth.pwd]);
+  // }, [queueNumber]);
 
   return (
     <div>
       <div style={{ textAlign: "center", fontSize: "50px", marginTop: "20px" }}>
-        <h1>Queue ticket number is {queueNumber}</h1>
+        {/* <h1>Queue ticket number is {queueNumber}</h1> */}
+        {/* <h1>Queue ticket number is </h1> */}
+        {queueNumber !== null ? (
+          <h1>Queue ticket number is {queueNumber}</h1>
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
       <Link to="/walkinHome">
         <button className="queue-button">Back</button>
--- react-frontend/src/components/Questionnaire/Questionnaire.jsx
+++ react-frontend/src/components/Questionnaire/Questionnaire.jsx
@@ -1,25 +1,135 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Axios from "axios";
 
 function Registration() {
     const [selectedButton1, setSelectedButton1] = useState(null);
     const [selectedButton2, setSelectedButton2] = useState(null);
     const [selectedButton3, setSelectedButton3] = useState(null);
     const navigate = useNavigate();
-
+    const [auth, setAuth] = useState({
+        user: sessionStorage.getItem("user"),
+        pwd: sessionStorage.getItem("pwd"),
+        personId: sessionStorage.getItem("person_id"),
+      });
+    
 
     const toggleBackgroundColor = (button, setButton) => {
         setButton((prevButton) => (prevButton === button ? null : button));
     };
 
+    
     const handleSubmit = () => {
         if (selectedButton1 === 'Yes' || selectedButton2 === 'Yes' || selectedButton3 === 'Yes') {
             alert('Please head to the front desk immediately');
             navigate('/walkinHome');
         }else{
+            getQueueNumber();
+            console.log("qNumber:" +queueNumber);
+            if (queueNumber === null) {
+                console.log("NULL - create new ticket now");
+                createNewTicket();
+            }else{
+                console.log("NOT NULL");
+            }
+
             navigate('/walkinHome/qticketpage');
         }
     };
+
+    const [queueNumber, setQueueNumber] = useState(null);
+    const [responseData, setResponseData] = useState(null);
+
+
+    const getQueueNumber = async () => {
+      try {
+        const response = await Axios.get(
+          `http://localhost:8080/api/v1/patients/${auth.personId}/getQtoday`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            auth: {
+              username: `${auth.user}`,
+              password: `${auth.pwd}`,
+            },
+          }
+        );
+  
+        if (response.data && response.data.dateTimeSlot) {
+          const qticket = response.data.dateTimeSlot.qticket;
+          if (qticket) {
+            setQueueNumber(qticket.ticketno);
+            console.log("success set qNumber:" + queueNumber);
+          }
+        }
+  
+      } catch (error) {
+        // Handle errors here
+        console.error("Error getting queue number:", error);
+      }
+  };
+  
+
+  useEffect(() => {
+    // Call fetchQueueNumber initially when the component mounts
+    getQueueNumber();
+
+    // Set up an interval to call fetchQueueNumber every, for example, 5 seconds (you can adjust the interval)
+    const intervalId = setInterval(getQueueNumber, 5000);
+    console.log(queueNumber);
+    // Clean up the interval when the component unmounts
+    return () => {
+        clearInterval(intervalId);
+    };
+}, []); // The empty dependency array ensures that this effect only runs once
+
+  const createNewTicket = async () => {
+    try {
+      const response = await Axios.post(
+        `http://localhost:8080/api/v1/patients/${auth.personId}/getnewQ`,
+        null, // You can pass data as the second argument if needed
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          auth: {
+            username: `${auth.user}`,
+            password: `${auth.pwd}`,
+          },
+        }
+      )
+      // ).then((response) => {
+        // Set the queue number in the state
+        // if (
+        //   response.data &&
+        //   response.data.qticket &&
+        //   response.data.qticket.ticketno
+        // ) {
+        //   setQueueNumber(response.data.qticket.ticketno);
+        // }
+        console.log(response.data);
+        const newQueueNumber = response.data.qTicket.ticketno;
+        sessionStorage.setItem("queueNumber", newQueueNumber);
+        sessionStorage.setItem("queueNumber", newQueueNumber);
+        setQueueNumber(newQueueNumber);
+        
+        console.log("New Ticket Number:", newQueueNumber);
+        console.log("current ticket number after creation: " + queueNumber);
+    } catch (error) {
+      // Handle errors here
+      const errorMessage = "Patient has already got a queue ticket for today";
+      console.log(error);
+      // if (error.response.data.message === errorMessage) {
+      //   getQueueNumber();
+      // }
+    //   console.error(
+    //     "Error creating a new ticket:",
+    //     error.response.data.message
+    //   );
+    }
+  };
+
 
     return (
         <div>
