c13e0655178eb2e2daea7f752765814a40570d38
Chua Zhi Yun <42872909+czyun44@users.noreply.github.com>
2023-11-06 15:57:07 +0800

    Update SecurityConfig.java

--- website/src/main/java/net/csd/website/security/SecurityConfig.java
+++ website/src/main/java/net/csd/website/security/SecurityConfig.java
@@ -64,7 +64,9 @@ public class SecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://203-web-dev.vercel.app");
             }
         };
     }
